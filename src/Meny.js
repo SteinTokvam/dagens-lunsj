0a351fcb74740d36fb7ccb4e9eb9c03e7f46253d
SteinTokvam <stto1210@gmail.com>
2023-06-22 10:00:48 +0200

    use json from repo

--- src/Meny.js
+++ src/Meny.js
@@ -1,93 +1,31 @@
 import { useEffect, useState } from 'react';
-import JSSoup from 'jssoup';
 import './Meny.css'
 
 export default function Meny() {
-    const[kroken, setKroken] = useState('')
     const[bright, setBright] = useState('')
  
-    function getMeny(url) {
-        const meny = fetch(url)
-        .then((res) => res.text())
-        .then((soup) => new JSSoup(soup).findAll('h3').slice(0, 5))
-        .then(meny => {
-        const res = []
-    
-        meny.forEach(element => {
-            const dayText = element.contents[0]._text
-            var lunsjText = ""
-            //console.log(element.contents[0]._text)
-            const day = element.contents[0].nextElement
-            day.contents.forEach(e => {
-            if(e._text !== undefined) {
-                lunsjText = lunsjText + '<br />' + e._text
-                //console.log(e._text)
-            }
-            })
-            res.push({day: dayText, meny: lunsjText})
-        });
-        return res
-        })
-        return meny
-    }
-
     useEffect(() => {
-        const print = (isKroken) => {
-            getMeny(isKroken ? 'https://mustadkantine.no/kafekroken' : 'https://mustadkantine.no/brightcafe').then(e => isKroken ? setKroken(e) : setBright(e))
+        function getMeny(url) {
+            fetch(url)
+            .then((res) => res.json())
+            .then(e => setBright(e))
         }
-        print(true)
-        print(false)
+        getMeny('https://raw.githubusercontent.com/SteinTokvam/dagens-lunsj/main/meny.json')
     }, []);
 
-    function format(string) {
-        return string
-        .replaceAll('&nbsp;', ' ')
-        .replaceAll('&aring;', 'å')
-        .replaceAll('&aelig;', 'æ')
-        .replaceAll('&amp;', '&')
-        .replaceAll('&oslash;', 'ø')
-    }
-
-    const dayOfWeek = new Date().getDay()
+    const dayOfWeek = new Date().getDay()-1
 
     return(<div className='float-container'>  
     <div className='float-child'>
-        <h1>Kroken</h1>
-        {kroken !== '' && dayOfWeek < 6 ? 
-        kroken.map((d, index) => {
-            const meny = d.meny.split('<br />').splice(2)
-            if(index === dayOfWeek-1) {
-                return(<>
-                    <h3 key={index}>{d.day}</h3>
-                    {
-                        meny.map((m, index) => {
-                            return(<p key={index}>{format(m)}</p>)
-                        })
-                    }
-                </>)
-            }
-            return <p key={index}></p>
-        }) 
-        : 'Stengt'}
+       {bright !== '' ? <h1>{bright[0].Kantine}</h1>: ''}
+       {bright !== '' ? <h3>{bright[0].meny[dayOfWeek].dag}</h3>: ''}
+       {bright !== '' ? <p>{bright[0].meny[dayOfWeek].meny}</p>: ''}
+        
     </div>
     <div className='float-child'>
-    <h1>Bright</h1>
-    {bright !== '' && dayOfWeek < 6 ? 
-        bright.map((d, index) => {
-            const meny = d.meny.split('<br />').splice(2)
-            if(index === dayOfWeek-1) {
-                return(<>
-                    <h3 key={index}>{d.day}</h3>
-                    {
-                        meny.map((m, index) => {
-                            return(<p key={index}>{format(m)}</p>)
-                        })
-                    }
-                </>)
-            }
-            return <p key={index}></p>
-        }) 
-        : 'Stengt'}
+        {bright !== '' ? <h1>{bright[1].Kantine}</h1>: ''}
+        {bright !== '' ? <h3>{bright[1].meny[dayOfWeek].dag}</h3>: ''}
+        {bright !== '' ? <p>{bright[1].meny[dayOfWeek].meny}</p>: ''}
     </div>
     </div>
     )
